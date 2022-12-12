c13293b3b81e5c1cd4361220fb01d75d349247c3
Henrique Teles Ribeiro <henrique@openlinesolutions.com.br>
2022-12-12 19:52:34 -0300

    Aula 14

--- start/routes.ts
+++ start/routes.ts
@@ -3,6 +3,8 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.post("/login", "AuthController.login");
 Route.post("/logout", "AuthController.logout")
 
+Route.post("/cliente/cadastro", "ClientesController.store");
+
 Route.group(() => {
   Route.get("/me", "AuthController.me");
 }).middleware("auth");
